67a6a1cc6186774707d19d1b8ab10230f223fd57
Pedja Muratovic <pmuratov1015@gmail.com>
2025-10-23 08:50:33 -0400

    cleaned up python cred gen

--- python/vfl/test_register_party.py
+++ python/vfl/test_register_party.py
@@ -3,7 +3,7 @@ import grpc
 from vfl.v1 import coordinator_pb2 as pb
 from vfl.v1 import coordinator_pb2_grpc as stubs
 
-def main():
+def get_channel_creds():
     base_path = Path(__file__).resolve().parents[2] / "certs"
     root_certs = (base_path / "dev-ca.crt").read_bytes()
     client_cert = (base_path / "client.crt").read_bytes()
@@ -14,7 +14,10 @@ def main():
         private_key=client_key,
         certificate_chain=client_cert,
     )
+    return creds
 
+def main():
+    creds = get_channel_creds()
     channel = grpc.secure_channel("localhost:8443", creds)
     client = stubs.CoordinatorStub(channel)
     resp = client.RegisterParty(pb.RegisterPartyRequest(party_id="partyA"))
